600b322d0c2e6684c54187ad7abf5a33452876fe
aboongm <aboongm@yahoo.com>
2022-12-02 18:49:03 +0530

    Add Create News in Profile dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   
   resources :news
   get 'pages/home'
+  get 'pages/dashboard'
   devise_for :users,
   path: '',
   path_names: { sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration' }
