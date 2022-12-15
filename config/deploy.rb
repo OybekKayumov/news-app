1dc784401a8c4fa08aaf5c306539da1b60960cc0
aboongm <aboongm@yahoo.com>
2022-12-15 16:13:44 +0530

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, "git@github.com:OybekKayumov/news-app.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, ENV['BRANCH'] if ENV['BRANCH']
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
